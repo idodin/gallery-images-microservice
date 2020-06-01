7493698473eb8db94b6286de4398e7708b117a49
Imad Dodin <imad.dodin@mail.mcgill.ca>
2020-06-01 00:25:58 -0400

    Pass Errors to Controller

--- src/util/filter.ts
+++ src/util/filter.ts
@@ -2,7 +2,6 @@ export const imageFilter = (req, file, cb) => {
     // Accept images only
     if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
         req.fileValidationError = 'Only image files are allowed!';
-        return cb(new Error('Only image files are allowed!'), false);
     }
     cb(null, true);
 }
